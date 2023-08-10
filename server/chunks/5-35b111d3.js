efabe4ff09ecb1475a63b287959b65510b28aa6c
Keith Chan <keithchan921@gmail.com>
2023-08-10 20:41:28 +0800

    Updates

---
+++ server/chunks/5-35b111d3.js
@@ -0,0 +1,9 @@
+const index = 5;
+let component_cache;
+const component = async () => component_cache ??= (await import('./_page.svelte-d97590a8.js')).default;
+const imports = ["_app/immutable/nodes/5.e5bf09a3.js","_app/immutable/chunks/index.39145068.js","_app/immutable/chunks/index.ebc39500.js","_app/immutable/chunks/ProgressBar.svelte_svelte_type_style_lang.3f221a79.js"];
+const stylesheets = ["_app/immutable/assets/ProgressBar.4f1e9ba5.css"];
+const fonts = [];
+
+export { component, fonts, imports, index, stylesheets };
+//# sourceMappingURL=5-35b111d3.js.map
---
+++ server/chunks/_page.svelte-211bbc1e.js
@@ -0,0 +1,19 @@
+import { c as create_ssr_component } from './index2-1f2322d7.js';
+import './ProgressBar.svelte_svelte_type_style_lang-da1c37ce.js';
+import './index-1e6528b2.js';
+
+const Page = create_ssr_component(($$result, $$props, $$bindings, slots) => {
+  return `<div class="previewer-viewport p-4 md:p-10 space-y-4 bg-gradient-to-br variant-gradient-primary-secondary "><div class="previewer-preview flex justify-center items-center mx-auto transition-[width] duration-200 w-full "><div class="w-full text-token grid grid-cols-1 md:grid-cols-2 gap-4"><div class="card variant-ghost-primary p-4 flex justify-center items-center"><span>Minimal</span></div>
+			<a class="card variant-ghost-primary card-hover overflow-hidden" href="/elements/cards"><header><div class="h2 text-center py-2">Header</div></header>
+			<hr class="opacity-50">
+			<section class="p-4 space-y-4"><h6 class="h6">Announcements</h6>
+				<h3 class="h3" data-toc-ignore="">This is Section</h3>
+				<article><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aspernatur provident eveniet eligendi cumque consequatur tempore sint nisi sapiente. Iste beatae laboriosam iure molestias cum expedita architecto itaque quae rem.</p></article></section>
+			<hr class="opacity-50">
+			<footer class="p-4 flex justify-start items-center space-x-4"><figure class="avatar flex aspect-square text-surface-50 font-semibold justify-center items-center overflow-hidden isolate bg-surface-400-500-token w-8 rounded-full" data-testid="avatar"><img src="https://images.unsplash.com/photo-1617296538902-887900d9b592?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzExMDB8&ixlib=rb-4.0.3&w=32&h=32&auto=format&fit=crop" alt="" style=""></figure>
+					<div class="flex-auto flex justify-between items-center"><h6 class="font-bold">Footer</h6>
+						<small>On 2023/8/4</small></div></footer></a></div></div></div>`;
+});
+
+export { Page as default };
+//# sourceMappingURL=_page.svelte-211bbc1e.js.map
